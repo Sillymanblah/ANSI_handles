4aa37820ee13e5de96f2c4dbda6a6787508c0075
John Grzegorczyk <jtgrzeg@gmail.com>
2025-02-21 00:22:09 -0600

    Updating the test to be a bit more rigorous.

--- src/tests/basic_test.cpp
+++ src/tests/basic_test.cpp
@@ -1,20 +1,26 @@
 
-#include "ANSI.hpp"
+#include "../ANSI.hpp"
 
 #include <iostream>
 
 int main()
 {
-    std::cout << ANSI::TEXT::GREEN << "Hello World!\n";
-    std::cout << ANSI::TEXT::RED << "REDDDDDDD RUMMM\n";
+    std::cout << "\nTesting text coloring...\n";
+    std::cout << ANSI::TEXT::GREEN << "Hello World!" << ANSI::TEXT::RESET;
 
-    std::cout << ANSI::BACKGROUND::CYAN << ANSI::TEXT::EFFECTS::BOLD_ON << ANSI::TEXT::BRIGHT_YELLOW << "Who let the dogs out?\n";
+    std::cout << "\nReset...\n\nNow testing background coloring...\n";
+    std::cout << ANSI::BACKGROUND::RED << "REDDDDDDD RUMMM" << ANSI::BACKGROUND::RESET;
 
-    std::cout << ANSI::DEFAULT << "I am back to normal!\n";
+    std::cout << "\nReset...\n\nNow testing text effects...\n";
+    std::cout << ANSI::TEXT::EFFECTS::BOLD_ON << "Who let the dogs out?" << ANSI::TEXT::EFFECTS::RESET;
 
-    std::cout << ANSI::COMMAND( ANSI::LAYER::BACKGROUND, ANSI::RGB_COLOR( 50, 80, 10 ) ) << "Some kind of gross green, maybe...\n";
+    std::cout << "\nReset...\n\nNow testing text all at once and reset...\n";
+    std::cout << ANSI::TEXT::BLUE << ANSI::BACKGROUND::BRIGHT_WHITE << ANSI::TEXT::EFFECTS::UNDERLINE_ON << "America!" << ANSI::RESET;
 
-    std::cout << ANSI::TEXT::MAGENTA << "Magenta is my favorite, but this looks like PINK!\n" << ANSI::FULL_RESET;
+    std::cout << "\nReset...\n\nNow testing custom command creation...\n";
+    std::cout << ANSI::COMMAND( ANSI::LAYER::BACKGROUND, ANSI::RGB_COLOR( 50, 80, 10 ) ) << "Some kind of gross green, maybe..." << ANSI::RESET;
 
-    return 1;
+    std::cout << "\nReset...\n\nTests complete!\n" << std::endl;
+
+    return 0;
 }
