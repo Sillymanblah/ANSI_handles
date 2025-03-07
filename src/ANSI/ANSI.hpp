86d98abe9dbf627d2f4178228302b75a5ec3875e
John Grzegorczyk <jtgrzeg@gmail.com>
2025-03-07 12:00:52 -0600

    Reformatting again since the default constructor will be called anyways and we do not need to use the assignment operator.

--- src/ANSI/ANSI.hpp
+++ src/ANSI/ANSI.hpp
@@ -11,95 +11,95 @@ namespace ANSI
         template < BYTE... _Data >
         using COMMAND = ::ANSI::COMMAND< FUNCTION::TEXT_EFFECT, _Data... >;
 
-        constexpr inline auto DEFAULT           = COMMAND<>();
-        constexpr inline auto RESET             = COMMAND< EFFECT::RESET_ALL >();
+        constexpr inline COMMAND<>                                     DEFAULT;
+        constexpr inline COMMAND< EFFECT::RESET_ALL >                  RESET;
 
         namespace FOREGROUND
         {
-            constexpr inline auto DEFAULT           = COMMAND< EFFECT::FOREGROUND + 1 >();
+            constexpr inline COMMAND< EFFECT::FOREGROUND + 1 >         DEFAULT;
 
-            constexpr inline auto BLACK             = COMMAND< EFFECT::FOREGROUND - 8 >();
-            constexpr inline auto RED               = COMMAND< EFFECT::FOREGROUND - 7 >();
-            constexpr inline auto GREEN             = COMMAND< EFFECT::FOREGROUND - 6 >();
-            constexpr inline auto BLUE              = COMMAND< EFFECT::FOREGROUND - 4 >();
-            constexpr inline auto YELLOW            = COMMAND< EFFECT::FOREGROUND - 5 >();
-            constexpr inline auto MAGENTA           = COMMAND< EFFECT::FOREGROUND - 3 >();
-            constexpr inline auto CYAN              = COMMAND< EFFECT::FOREGROUND - 2 >();
-            constexpr inline auto WHITE             = COMMAND< EFFECT::FOREGROUND - 1 >();
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 8 >         BLACK;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 7 >         RED;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 6 >         GREEN;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 4 >         BLUE;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 5 >         YELLOW;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 3 >         MAGENTA;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 2 >         CYAN;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 1 >         WHITE;
     
             // NOTE: Bright colors can also usually be achieved via layering BOLD with the text color.
-            constexpr inline auto BRIGHT_BLACK      = COMMAND< EFFECT::FOREGROUND - 8 + 60 >();
-            constexpr inline auto BRIGHT_RED        = COMMAND< EFFECT::FOREGROUND - 7 + 60 >();
-            constexpr inline auto BRIGHT_GREEN      = COMMAND< EFFECT::FOREGROUND - 6 + 60 >();
-            constexpr inline auto BRIGHT_BLUE       = COMMAND< EFFECT::FOREGROUND - 4 + 60 >();
-            constexpr inline auto BRIGHT_YELLOW     = COMMAND< EFFECT::FOREGROUND - 5 + 60 >();
-            constexpr inline auto BRIGHT_MAGENTA    = COMMAND< EFFECT::FOREGROUND - 3 + 60 >();
-            constexpr inline auto BRIGHT_CYAN       = COMMAND< EFFECT::FOREGROUND - 2 + 60 >();
-            constexpr inline auto BRIGHT_WHITE      = COMMAND< EFFECT::FOREGROUND - 1 + 60 >();
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 8 + 60 >    BRIGHT_BLACK;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 7 + 60 >    BRIGHT_RED;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 6 + 60 >    BRIGHT_GREEN;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 4 + 60 >    BRIGHT_BLUE;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 5 + 60 >    BRIGHT_YELLOW;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 3 + 60 >    BRIGHT_MAGENTA;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 2 + 60 >    BRIGHT_CYAN;
+            constexpr inline COMMAND< EFFECT::FOREGROUND - 1 + 60 >    BRIGHT_WHITE;
         }
         // namespace FOREGROUND
 
         namespace EFFECTS
         {
-            constexpr inline auto UNDERLINE_ON      = COMMAND< EFFECT::UNDERLINE >();
-            constexpr inline auto UNDERLINE_X2      = COMMAND< EFFECT::DOUBLE_UNDERLINE >();
-            constexpr inline auto UNDERLINE_OFF     = COMMAND< EFFECT::RESET_UNDERLINE >();
+            constexpr inline COMMAND< EFFECT::UNDERLINE >              UNDERLINE_ON;
+            constexpr inline COMMAND< EFFECT::DOUBLE_UNDERLINE >       UNDERLINE_X2;
+            constexpr inline COMMAND< EFFECT::RESET_UNDERLINE >        UNDERLINE_OFF;
             
-            constexpr inline auto OVERLINE_ON       = COMMAND< EFFECT::OVERLINE >();
-            constexpr inline auto OVERLINE_OFF      = COMMAND< EFFECT::RESET_OVERLINE >();
+            constexpr inline COMMAND< EFFECT::OVERLINE >               OVERLINE_ON;
+            constexpr inline COMMAND< EFFECT::RESET_OVERLINE >         OVERLINE_OFF;
 
-            constexpr inline auto BLINKING_ON       = COMMAND< EFFECT::BLINKING >();
-            constexpr inline auto BLINKING_OFF      = COMMAND< EFFECT::RESET_BLINKING >();
+            constexpr inline COMMAND< EFFECT::BLINKING >               BLINKING_ON;
+            constexpr inline COMMAND< EFFECT::RESET_BLINKING >         BLINKING_OFF;
 
-            constexpr inline auto BOLD_ON           = COMMAND< EFFECT::BOLD >();
-            constexpr inline auto BOLD_OFF          = COMMAND< EFFECT::RESET_BOLD >();
+            constexpr inline COMMAND< EFFECT::BOLD >                   BOLD_ON;
+            constexpr inline COMMAND< EFFECT::RESET_BOLD >             BOLD_OFF;
 
-            constexpr inline auto DIM_ON            = COMMAND< EFFECT::DIM >();
-            constexpr inline auto DIM_OFF           = COMMAND< EFFECT::RESET_DIM >();
+            constexpr inline COMMAND< EFFECT::DIM >                    DIM_ON;
+            constexpr inline COMMAND< EFFECT::RESET_DIM >              DIM_OFF;
 
-            constexpr inline auto ITALIC_ON         = COMMAND< EFFECT::ITALIC >();
-            constexpr inline auto ITALIC_OFF        = COMMAND< EFFECT::RESET_ITALIC >();
+            constexpr inline COMMAND< EFFECT::ITALIC >                 ITALIC_ON;
+            constexpr inline COMMAND< EFFECT::RESET_ITALIC >           ITALIC_OFF;
 
-            constexpr inline auto OPEN_FRAME        = COMMAND< EFFECT::FRAME_OPEN >();
-            constexpr inline auto CLOSE_FRAME       = COMMAND< EFFECT::FRAME_CLOSE >();
+            constexpr inline COMMAND< EFFECT::FRAME_OPEN >             OPEN_FRAME;
+            constexpr inline COMMAND< EFFECT::FRAME_CLOSE >            CLOSE_FRAME;
 
-            constexpr inline auto HIDE              = COMMAND< EFFECT::HIDDEN >();
-            constexpr inline auto UNHIDE            = COMMAND< EFFECT::RESET_HIDDEN >();
+            constexpr inline COMMAND< EFFECT::HIDDEN >                 HIDE;
+            constexpr inline COMMAND< EFFECT::RESET_HIDDEN >           UNHIDE;
             
-            constexpr inline auto INVERT_ON         = COMMAND< EFFECT::INVERSE >();
-            constexpr inline auto INVERT_OFF        = COMMAND< EFFECT::RESET_INVERSE >();
+            constexpr inline COMMAND< EFFECT::INVERSE >                INVERT_ON;
+            constexpr inline COMMAND< EFFECT::RESET_INVERSE >          INVERT_OFF;
             
-            constexpr inline auto STRIKE_ON         = COMMAND< EFFECT::STRIKETHROUGH >();
-            constexpr inline auto STRIKE_OFF        = COMMAND< EFFECT::RESET_STRIKETHROUGH >();
+            constexpr inline COMMAND< EFFECT::STRIKETHROUGH >          STRIKE_ON;
+            constexpr inline COMMAND< EFFECT::RESET_STRIKETHROUGH >    STRIKE_OFF;
             
-            constexpr inline auto SUB_ON            = COMMAND< EFFECT::SUBSCRIPT >();
-            constexpr inline auto SUPER_ON          = COMMAND< EFFECT::SUPERSCRIPT >();
-            constexpr inline auto SCRIPTS_OFF       = COMMAND< EFFECT::RESET_SCRIPT >();
+            constexpr inline COMMAND< EFFECT::SUBSCRIPT >              SUB_ON;
+            constexpr inline COMMAND< EFFECT::SUPERSCRIPT >            SUPER_ON;
+            constexpr inline COMMAND< EFFECT::RESET_SCRIPT >           SCRIPTS_OFF;
         }
         // namespace EFFECTS
 
         namespace BACKGROUND
         {
-            constexpr inline auto DEFAULT           = COMMAND< EFFECT::BACKGROUND + 1 >();
+            constexpr inline COMMAND< EFFECT::BACKGROUND + 1 >         DEFAULT;
 
-            constexpr inline auto BLACK             = COMMAND< EFFECT::BACKGROUND - 8 >();
-            constexpr inline auto RED               = COMMAND< EFFECT::BACKGROUND - 7 >();
-            constexpr inline auto GREEN             = COMMAND< EFFECT::BACKGROUND - 6 >();
-            constexpr inline auto BLUE              = COMMAND< EFFECT::BACKGROUND - 4 >();
-            constexpr inline auto YELLOW            = COMMAND< EFFECT::BACKGROUND - 5 >();
-            constexpr inline auto MAGENTA           = COMMAND< EFFECT::BACKGROUND - 3 >();
-            constexpr inline auto CYAN              = COMMAND< EFFECT::BACKGROUND - 2 >();
-            constexpr inline auto WHITE             = COMMAND< EFFECT::BACKGROUND - 1 >();
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 8 >         BLACK;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 7 >         RED;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 6 >         GREEN;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 4 >         BLUE;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 5 >         YELLOW;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 3 >         MAGENTA;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 2 >         CYAN;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 1 >         WHITE;
 
             // NOTE: Bright colors can also usually be achieved via layering BOLD with the text color.
-            constexpr inline auto BRIGHT_BLACK      = COMMAND< EFFECT::BACKGROUND - 8 + 60 >();
-            constexpr inline auto BRIGHT_RED        = COMMAND< EFFECT::BACKGROUND - 7 + 60 >();
-            constexpr inline auto BRIGHT_GREEN      = COMMAND< EFFECT::BACKGROUND - 6 + 60 >();
-            constexpr inline auto BRIGHT_BLUE       = COMMAND< EFFECT::BACKGROUND - 4 + 60 >();
-            constexpr inline auto BRIGHT_YELLOW     = COMMAND< EFFECT::BACKGROUND - 5 + 60 >();
-            constexpr inline auto BRIGHT_MAGENTA    = COMMAND< EFFECT::BACKGROUND - 3 + 60 >();
-            constexpr inline auto BRIGHT_CYAN       = COMMAND< EFFECT::BACKGROUND - 2 + 60 >();
-            constexpr inline auto BRIGHT_WHITE      = COMMAND< EFFECT::BACKGROUND - 1 + 60 >();
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 8 + 60 >    BRIGHT_BLACK;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 7 + 60 >    BRIGHT_RED;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 6 + 60 >    BRIGHT_GREEN;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 4 + 60 >    BRIGHT_BLUE;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 5 + 60 >    BRIGHT_YELLOW;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 3 + 60 >    BRIGHT_MAGENTA;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 2 + 60 >    BRIGHT_CYAN;
+            constexpr inline COMMAND< EFFECT::BACKGROUND - 1 + 60 >    BRIGHT_WHITE;
         }
         // namespace BACKGROUND
     }
