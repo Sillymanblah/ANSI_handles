05190ffc164144518634805c5b979f97f95613e9
John Grzegorczyk <jtgrzeg@gmail.com>
2025-03-07 13:57:14 -0600

    Updated the basic test to be more intensive of ANSI and check the new functionality.

--- src/tests/basic_test.cpp
+++ src/tests/basic_test.cpp
@@ -6,5 +6,8 @@ int main()
 {
 	using namespace ANSI;
 
-	std::cout << TEXT::FOREGROUND::BLUE << "Blue text?";
+	std::cout << TEXT::FOREGROUND::BLUE + TEXT::BACKGROUND::BRIGHT_RED + TEXT::EFFECTS::UNDERLINE_ON << "Blue text, red background, and underline..." << TEXT::DEFAULT << '\n';
+	std::cout << "Text has been defaulted...\n";
+	std::cout << ANSI::merge_v< TEXT::COMMAND< 34 >, TEXT::COMMAND< 101 >, TEXT::COMMAND< 4 > > << "Same thing done with ANSI::merge..." << TEXT::RESET << '\n';
+	std::cout << "Text has been reset..." << std::endl;
 }
